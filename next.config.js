adb8eda900a84b3fe0fd75c01a2e839456ea8459
Alex Ficachi <ficachi@Alexs-MBP.lan>
2022-09-25 13:23:03 -0700

    redesign

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+// next.config.js
+module.exports = {
+  images: {
+    domains: ['assets.maccarianagency.com'],
+  },
 }
-
-module.exports = nextConfig
